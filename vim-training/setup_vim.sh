b3c7c88999cc158aef9ccef33d56fac5bb6d5a64
Jemwel O. Autor <jemwelautor@gmail.com>
2023-07-24 11:22:34 +0800

    Update setup_vim.sh

--- vim-training/setup_vim.sh
+++ vim-training/setup_vim.sh
@@ -6,10 +6,15 @@ sudo apt install ack;
 sudo apt install silversearcher-ag;
 
 #create directory ~/.vim/swapfiles
-
-#wget my/git/hub/url/.vimrc
+mkdir .vim
+mkdir ~/.vim/swapfiles
 
 #curl vim plug
 echo "Installing vim plug"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+#Download .vimrc
+wget https://raw.githubusercontent.com/joautor/lab/main/vim-training/.vimrc;
+
+
 
