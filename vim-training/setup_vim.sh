5e661bfbfb63d4a67d3fe83a5940422aa8f90eb0
Jemwel O. Autor <jemwelautor@gmail.com>
2025-05-15 16:02:10 +0800

    Update setup_vim.sh

--- vim-training/setup_vim.sh
+++ vim-training/setup_vim.sh
@@ -2,22 +2,57 @@
 cd ~
 #Install necessary packages
 echo "[INFO] Installing packages needed for vim plugins"
-echo "[INF0] Installing ack"
+
+echo "[INF0] Installing ack ..."
 sudo apt install ack -y
-echo "[INFO] Installing silversearcher-ag"
+
+echo "[INFO] Installing silversearcher-ag ..."
 sudo apt install silversearcher-ag -y
 
-#create directory ~/.vim/swapfiles
-echo "[INFO] Creating ~/.vim/swapfiles"
-mkdir -p ~/.vim/swapfiles
+echo "[INFO] Installing curl ..."
+sudo apt install curl -y
 
-#curl vim plug
-echo "[INFO] Installing vim plug"
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+echo "[INFO] Installing wget ..."
+sudo apt install wget -y
 
-#Download .vimrc
-echo "[INFO] Downloading .vimrc"
-wget https://raw.githubusercontent.com/joautor/lab/main/vim-training/.vimrc;
+# Setup swap directory
+SWAP_DIR=~/.vim/swapfiles
+if [[ ! -d ${SWAP_DIR} ]]; then
+    echo "[INFO] Creating ${SWAP_DIR} ..."
+    mkdir -p ${SWAP_DIR}
+else
+    echo "[INFO] ${SWAP_DIR} already exists"
+fi
+
+#Install vim plug
+VIM_PLUG_LOCATION=~/.vim/autoload/plug.vim
+if [ ! -f ${VIM_PLUG_LOCATION} ]; then
+    echo "[INFO] Installing vim plug ..."
+    curl -fLo ${VIM_PLUG_LOCATION} --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    if [ $? -ne 0]; then
+        echo "[ERROR] Failed to install vim plug."
+        exit 1
+    fi
+else
+    echo "[INFO] vim plug is already installed."
+fi
+
+#Setup .vimrc
+if [ -f .vimrc ]; then
+    echo "[WARNING] .vimrc file already exists. This will be overwritten."
+    read -p "Do you wish to continue? (y/n): " confirm
+    if [ $confirm != "y" ]; then
+        exit 1
+    fi
+    echo "[INFO] removing existing .vimrc ..."
+    rm .vimrc
+fi
+echo "[INFO] Downloading remote .vimrc ..."
+wget https://raw.githubusercontent.com/joautor/lab/main/vim-training/.vimrc
+if [ $? -ne 0 ]; then
+    echo "[ERROR] unable to download .vimrc"
+    exit 1
+fi
 
 echo "[INFO] Installing vim plugins..."
 vim +'PlugInstall --sync' +qa
